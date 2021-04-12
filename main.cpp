0e85d995a44c418f8ce44bfaf996012883d9168f
Yassine Mejri <38594757+YassineMEJRI@users.noreply.github.com>
2021-04-12 22:50:44 +0100

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Game.h"
+
+
+
+using namespace std;
+
+int main()
+{
+    string c;
+    int a,b;
+    cout<<"tapez Start to initializate the game"<<endl;
+    cin>> c;
+    if (c=="start"){
+        system("CLS");
+        string c="";
+        Game k;
+        k.initiate();
+        int nb = 2 ;
+        do {
+            k.jouerTour(k,nb);
+        }while(k.othellier.getnbPionsTotale()<64);
+        }
+    return 0;
+}
